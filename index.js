5bbb3be8f636d3a776740732cf4ef65c5449d9e3
RUIZFPS <lucasruiz1025@gmail.com>
2023-05-16 13:41:31 -0300

    1.22

--- index.js
+++ index.js
@@ -4,60 +4,61 @@ const GlobalOffensive = require('globaloffensive');
 const SteamCommunity = require('steamcommunity');
 const Discord = require('discord.js');
 const config = require('./config.json');
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const { EmbedBuilder } = require('discord.js');
+const { Client, Intents, EmbedBuilder } = require('discord.js');
 
-// Discord v14
 const client = new Discord.Client({ intents: [
   Discord.GatewayIntentBits.Guilds,
   Discord.GatewayIntentBits.GuildMessages
-]})
+]});
+const steamUsers = [];
 
-const steamUser = new SteamUser({
-  promptSteamGuardCode: false,
-});
+for (const account of config.steam.accounts) {
+  const steamUser = new SteamUser({ promptSteamGuardCode: false });
 
-const csgo = new GlobalOffensive(steamUser);
-const community = new SteamCommunity();
+  steamUser.logOn({
+    accountName: account.username,
+    password: account.password,
+    twoFactorCode: SteamTotp.generateAuthCode(account.shared_secret),
+  });
 
-steamUser.logOn({
-  accountName: config.steam.username,
-  password: config.steam.password,
-  twoFactorCode: SteamTotp.generateAuthCode(config.steam.shared_secret),
-});
+  steamUser.on('loggedOn', () => {
+    console.log(`Logged in to Steam account: ${account.display_name}`);
+    steamUser.setPersona(SteamUser.EPersonaState.Online);
+    steamUser.gamesPlayed([730]);
+  });
 
-steamUser.on('loggedOn', () => {
-  console.log('Logged in to Steam!');
-  steamUser.setPersona(SteamUser.EPersonaState.Online);
-  steamUser.gamesPlayed([730]);
-});
+  const csgo = new GlobalOffensive(steamUser);
+  const community = new SteamCommunity();
 
-csgo.on('connectedToGC', () => {
-  console.log('Connected to CS:GO Game Coordinator!');
-});
+  csgo.on('connectedToGC', () => {
+    console.log(`Connected to CS:GO Game Coordinator - ${account.display_name}`);
+  });
 
-steamUser.on('webSession', (sessionID, cookies) => {
-  community.setCookies(cookies);
-  community.startConfirmationChecker(10000, config.steam.identity_secret);
-});
+  steamUser.on('webSession', (sessionID, cookies) => {
+    community.setCookies(cookies);
+    community.startConfirmationChecker(10000, account.identity_secret);
+  });
 
-csgo.on('itemAcquired', item => {
-  console.log('Você recebeu um novo item! ');
-  const channel = client.channels.cache.get(config.discord.channel_id);
+  csgo.on('itemAcquired', (item) => {
+    console.log(`A conta ${account.display_name} recebeu um novo item!`);
+    const channel = client.channels.cache.get(config.discord.channel_id);
 
-  if (!channel) {
-    console.error(`Channel ${config.discord.channel_id} not found.`);
-    return;
-  }
+    if (!channel) {
+      console.error(`Channel ${config.discord.channel_id} not found.`);
+      return;
+    }
 
-  const embed = new Discord.EmbedBuilder()
-    .setColor('#ff0000')
-    .setTitle('Você recebeu um novo item!')
-    .setDescription(`Voce recebeu um novo item`)
+    const embed = new Discord.EmbedBuilder()
+      .setColor('#ff0000')
+      .setTitle('Você recebeu um novo item!')
+      .setDescription(`Você recebeu um novo item na conta: ${account.display_name}`);
 
-  channel.send({ embeds: [embed] })
-    .catch(console.error);
-});
+    channel.send({ embeds: [embed] })
+      .catch(console.error);
+  });
+
+  steamUsers.push({ steamUser, csgo, community, account });
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
