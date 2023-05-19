d5f97120e30a3f6ca008699e1b88895fd5548504
Lunixyz <otakinhuhdev@gmail.com>
2023-05-19 15:17:20 -0300

    Código "Limpado".

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-CSGO-ItemAlert
+# Notificador de novos Items
+Quer ser notificado quando recebe novos Item em seu inventário do CS:GO?
+Utilize esse BOT então! É necessário apenas inserir alguns dados na `config.json` e pronto!
--- index.js
+++ index.js
@@ -1,67 +1,18 @@
-const SteamUser = require('steam-user');
-const SteamTotp = require('steam-totp');
-const GlobalOffensive = require('globaloffensive');
-const SteamCommunity = require('steamcommunity');
-const Discord = require('discord.js');
 const config = require('./config.json');
-const { Client, Intents, EmbedBuilder } = require('discord.js');
+const { Client } = require('discord.js');
+const { newSteamUser } = require("./SteamUser")
 
-const client = new Discord.Client({ intents: [
-  Discord.GatewayIntentBits.Guilds,
-  Discord.GatewayIntentBits.GuildMessages
-]});
-const steamUsers = [];
+const client = new Client({ intents: 3276799 });
 
-for (const account of config.steam.accounts) {
-  const steamUser = new SteamUser({ promptSteamGuardCode: false });
+client.once('ready', () => console.log(`Logado no Discord como ${client.user.tag}!`));
 
-  steamUser.logOn({
-    accountName: account.username,
-    password: account.password,
-    twoFactorCode: SteamTotp.generateAuthCode(account.shared_secret),
-  });
-
-  steamUser.on('loggedOn', () => {
-    console.log(`Logged in to Steam account: ${account.display_name}`);
-    steamUser.setPersona(SteamUser.EPersonaState.Online);
-    steamUser.gamesPlayed([730]);
-  });
-
-  const csgo = new GlobalOffensive(steamUser);
-  const community = new SteamCommunity();
-
-  csgo.on('connectedToGC', () => {
-    console.log(`Connected to CS:GO Game Coordinator - ${account.display_name}`);
-  });
-
-  steamUser.on('webSession', (sessionID, cookies) => {
-    community.setCookies(cookies);
-    community.startConfirmationChecker(10000, account.identity_secret);
-  });
-
-  csgo.on('itemAcquired', (item) => {
-    console.log(`A conta ${account.display_name} recebeu um novo item!`);
-    const channel = client.channels.cache.get(config.discord.channel_id);
-
-    if (!channel) {
-      console.error(`Channel ${config.discord.channel_id} not found.`);
-      return;
-    }
-
-    const embed = new Discord.EmbedBuilder()
-      .setColor('#ff0000')
-      .setTitle('Você recebeu um novo item!')
-      .setDescription(`Você recebeu um novo item na conta: ${account.display_name}`);
-
-    channel.send({ embeds: [embed] })
-      .catch(console.error);
-  });
-
-  steamUsers.push({ steamUser, csgo, community, account });
-}
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+for (const conta of config.steam.accounts)
+  new newSteamUser().create(
+    {
+      username: conta.username,
+      password: conta.password,
+      channelId: config.discord.channel_id,
+      client: client
+    })
 
 client.login(config.discord.token);
